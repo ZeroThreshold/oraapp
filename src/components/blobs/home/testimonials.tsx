631d2df832ffc7d2d75da2a9d9d1c332a513e6a2
roverflow <roverflow5@gmail.com>
2024-05-20 15:19:55 +0530

    fix instagram

--- src/components/blobs/home/testimonials.tsx
+++ src/components/blobs/home/testimonials.tsx
@@ -18,7 +18,9 @@ const Testimonials = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {linksinsta.map((link) => (
-          <InstagramEmbed url={link} key={link} />
+          <div key={link}>
+            <InstagramEmbed url={link} className="w-full object-fit" />
+          </div>
         ))}
       </div>
     </div>
