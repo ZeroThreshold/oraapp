37e40a709450935a4727642bec2d5cb5b1b1888c
roverflow <roverflow5@gmail.com>
2024-05-24 21:25:55 +0530

    image fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,22 @@ import LocationComp from "@/components/blobs/home/location";
 import Testimonials from "@/components/blobs/home/testimonials";
 import WeDo from "@/components/blobs/home/wedo";
 import VideoPlayer from "@/components/blocks/home/videoplayer";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <VideoPlayer />
       <WeDo />
-      <section
-        className="section py-5 h-[100vh] parallax w-[99.9vw]"
+      <div
+        className="section py-5 h-[100vh] parallax w-[100vw]"
         style={{ backgroundImage: `url("/images/general/para1.jpg")` }}
-      ></section>
+      ></div>
       <LocationComp />
-      <section
-        className="section py-5 h-[100vh] parallax w-[99.9vw]"
+      <div
+        className="section py-5 h-[100vh] parallax w-[100vw]"
         style={{ backgroundImage: `url("/images/general/para2.jpg")` }}
-      ></section>
+      ></div>
       <Testimonials />
     </main>
   );
